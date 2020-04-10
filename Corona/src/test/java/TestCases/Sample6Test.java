7a066c2bc4c492d41c9e96280c3dd19911803a85
jai kumar <jai kumar@LAPTOP-E195DL4B>
2020-04-10 08:10:52 +0530

    first commit

---
+++ Corona/src/test/java/TestCases/Sample6Test.java
@@ -0,0 +1,28 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class Sample6Test {
+
+	@Test
+	public void bike() {
+		
+		System.out.println("Bike");
+	}
+	@Test
+	public void cycle() {
+		
+		System.out.println("cycle");
+	}
+	@Test
+	public void scooty() {
+		
+		System.out.println("scooty");
+	}
+	@Test
+	public void bullet() {
+		
+		System.out.println("bullet");
+	}
+	
+}
---
+++ Corona/src/test/java/TestCases/Sample7Test.java
@@ -0,0 +1,28 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class Sample7Test {
+	
+	@Test
+	public void bike() {
+		
+		System.out.println("Bike");
+	}
+	@Test
+	public void bike1() {
+		
+		System.out.println("Bike1");
+	}
+
+	@Test
+	public void bike2() {
+		
+		System.out.println("Bike2");
+	}
+	@Test
+	public void bike3() {
+		
+		System.out.println("Bike3");
+	}
+}
